361c2fc3b09124be1606ad24fa94d06e4fd67854
SergeyRSV <vbhyjnen123>
2019-07-06 17:08:51 +0300

    upload

---
+++ TeleBot.py
@@ -0,0 +1,20 @@
+import requests
+import telebot;
+
+token = '898400731:AAF3bs_P6pXMPMP5kg2b0lfkilsMulYKos0'
+bot = telebot.TeleBot(token);
+
+
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
+bot.polling(none_stop=True, interval=0)
